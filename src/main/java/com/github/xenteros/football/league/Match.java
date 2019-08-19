bf5fe201d53ba25d6c0dcdb113479a7166cfa1cb
xenteros <adan@gurgul.com.pl>
2019-08-19 18:38:58 +0200

    Implement Team and Match

--- src/main/java/com/github/xenteros/football/league/Match.java
+++ src/main/java/com/github/xenteros/football/league/Match.java
@@ -7,4 +7,40 @@ package com.github.xenteros.football.league;
  */
 class Match {
 
+    private Team host;
+    private Team away;
+    private String result;
+
+    public Match() {
+    }
+
+    public Match(Team host, Team away, String result) {
+        this.host = host;
+        this.away = away;
+        this.result = result;
+    }
+
+    public Team getHost() {
+        return host;
+    }
+
+    public void setHost(Team host) {
+        this.host = host;
+    }
+
+    public Team getAway() {
+        return away;
+    }
+
+    public void setAway(Team away) {
+        this.away = away;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
 }
--- src/main/java/com/github/xenteros/football/league/Team.java
+++ src/main/java/com/github/xenteros/football/league/Team.java
@@ -1,9 +1,46 @@
 package com.github.xenteros.football.league;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Przechowuje listę zawodników (załóżmy sobie, że imię i nazwisko zawodnika jest unikalne),
  * ma nazwę.
  */
 class Team {
+
+    private List<String> players;
+    private String name;
+
+    public Team() {
+    }
+
+    public Team(List<String> players, String name) {
+        this.players = players;
+        this.name = name;
+    }
+
+    public static Team languages() {
+        Team team = new Team();
+        team.setName("languages");
+        team.setPlayers(Arrays.asList("Java", "Python", "C++"));
+        return team;
+    }
+
+    public List<String> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<String> players) {
+        this.players = players;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
